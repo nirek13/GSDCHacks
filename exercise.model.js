64810d20a0110cabd26ff1c4134520c4a5ac66d2
James Cao <127159112+ThePathOfLeastResistance@users.noreply.github.com>
2024-05-13 09:08:44 -0400

    Add files via upload

---
+++ exercise.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+  username: { type: String, required: true },
+  description: { type: String, required: true },
+});
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+module.exports = Exercise;
